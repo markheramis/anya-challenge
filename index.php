27fe46d26959454b1955483d9b927c6782c088fd
Mark Clu Heramis <chumheramis@gmail.com>
2018-02-07 21:19:05 +0800

    to accomodate for console
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ for ($i = 1; $i < 10; $i++) {
     $nums = $nums . $i;
     $out = $nums;
     if($i != 1) $out = $out . substr(strrev($out),1);
-    echo '<p style="text-align:center;">' . $out . '</p>';
+    #echo '<p style="text-align:center;">' . $out . '</p>';
+    print($out . "\n");
 }
