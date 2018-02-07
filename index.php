00889ea93a43f90013a89dbbbc6d99007f7dfc18
Mark Clu Heramis <chumheramis@gmail.com>
2018-02-07 21:14:02 +0800

    initial commit
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$nums = '';
+for ($i = 1; $i < 10; $i++) {
+    $nums = $nums . $i;
+    $out = $nums;
+    if($i != 1) $out = $out . substr(strrev($out),1);
+    echo '<p style="text-align:center;">' . $out . '</p>';
+}
